3665980901a3405b430846a2b1127a988b9c8be4
Matthias <me@ptweety.net>
2022-03-26 12:48:45 +0100

    Update merge.js
    linting with `eslint` and `prettier`

--- huemagic/utils/merge.js
+++ huemagic/utils/merge.js
@@ -1,34 +1,26 @@
-function mergeDeep(...objects)
-{
-    // CLEANUP FROM HTTP NODE
-    delete objects.req;
-    delete objects.res;
+function mergeDeep(...objects) {
+	// CLEANUP FROM HTTP NODE
+	delete objects.req;
+	delete objects.res;
 
-    // BEGIN MERGING …
-    const isObject = obj => obj && typeof obj === 'object';
-    return objects.reduce((prev, obj) =>
-    {
-        Object.keys(obj).forEach(key =>
-        {
-            const pVal = prev[key];
-            const oVal = obj[key];
+	// BEGIN MERGING …
+	const isObject = (obj) => obj && typeof obj === 'object';
+	return objects.reduce((prev, obj) => {
+		Object.keys(obj).forEach((key) => {
+			const pVal = prev[key];
+			const oVal = obj[key];
 
-            if(Array.isArray(pVal) && Array.isArray(oVal))
-            {
-                prev[key] = pVal.concat(...oVal);
-            }
-            else if (isObject(pVal) && isObject(oVal))
-            {
-                prev[key] = mergeDeep(pVal, oVal);
-            }
-            else
-            {
-                prev[key] = oVal;
-            }
-        });
+			if (Array.isArray(pVal) && Array.isArray(oVal)) {
+				prev[key] = pVal.concat(...oVal);
+			} else if (isObject(pVal) && isObject(oVal)) {
+				prev[key] = mergeDeep(pVal, oVal);
+			} else {
+				prev[key] = oVal;
+			}
+		});
 
-        return prev;
-    }, {});
+		return prev;
+	}, {});
 }
 
 // EXPORT
