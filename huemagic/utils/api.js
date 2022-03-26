4d4879250ef1bacb1af53fe17fb8309536999138
Matthias <me@ptweety.net>
2022-03-26 12:48:39 +0100

    Update api.js
    linting with `eslint` and `prettier`

--- huemagic/utils/api.js
+++ huemagic/utils/api.js
@@ -3,124 +3,103 @@ const dayjs = require('dayjs');
 const https = require('https');
 const EventSource = require('eventsource');
 
-function API()
-{
+function API() {
 	// EVENTS
 	this.events = {};
 
 	//
 	// INITIALIZE
-	this.init = function({ config = null })
-	{
+	this.init = function ({ config = null }) {
+		// eslint-disable-next-line no-unused-vars
 		const scope = this;
 
 		// GET BRIDGE
-		return new Promise(function(resolve, reject)
-		{
-			if(!config)
-			{
-				reject("Bridge is not configured!");
+		return new Promise(function (resolve, reject) {
+			if (!config) {
+				reject('Bridge is not configured!');
 				return false;
 			}
 
 			// GET BRIDGE INFORMATION
 			axios({
-				"method": "GET",
-				"url": "https://" + config.bridge + "/api/config",
-				"headers": { "Content-Type": "application/json; charset=utf-8" },
-				"httpsAgent": new https.Agent({ rejectUnauthorized: false }),
+				method: 'GET',
+				url: 'https://' + config.bridge + '/api/config',
+				headers: { 'Content-Type': 'application/json; charset=utf-8' },
+				httpsAgent: new https.Agent({ rejectUnauthorized: false }),
 			})
-			.then(function(response)
-			{
-				resolve(response.data);
-			})
-			.catch(function(error)
-			{
-				reject(error);
-			});
+				.then(function (response) {
+					resolve(response.data);
+				})
+				.catch(function (error) {
+					reject(error);
+				});
 		});
-	}
+	};
 
 	//
 	// MAKE A REQUEST
-	this.request = function({ config = null, method = 'GET', resource = null, data = null, version = 2 })
-	{
+	this.request = function ({ config = null, method = 'GET', resource = null, data = null, version = 2 }) {
+		// eslint-disable-next-line no-unused-vars
 		const scope = this;
-		return new Promise(function(resolve, reject)
-		{
-			if(!config)
-			{
-				reject("Bridge is not configured!");
+		return new Promise(function (resolve, reject) {
+			if (!config) {
+				reject('Bridge is not configured!');
 				return false;
 			}
 
 			// BUILD REQUEST OBJECT
-			var request = {
-				"method": method,
-				"url": "https://" + config.bridge,
-				"headers": {
-					"Content-Type": "application/json; charset=utf-8",
-					"hue-application-key": config.key
+			let request = {
+				method: method,
+				url: 'https://' + config.bridge,
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8',
+					'hue-application-key': config.key,
 				},
-				"httpsAgent": new https.Agent({ rejectUnauthorized: false }), // Node is somehow not able to parse the official Philips Hue PEM
+				httpsAgent: new https.Agent({ rejectUnauthorized: false }), // Node is somehow not able to parse the official Philips Hue PEM
 			};
 
 			// HAS RESOURCE? -> APPEND
-			if(resource !== null)
-			{
-				if(version === 2)
-				{
-					resource = (resource !== "all") ? "/"+resource : "";
-					request['url'] += "/clip/v2/resource" + resource;
-				}
-				else if(version === 1)
-				{
-					request['url'] += "/api/" + config.key + resource;
+			if (resource !== null) {
+				if (version === 2) {
+					resource = resource !== 'all' ? '/' + resource : '';
+					request['url'] += '/clip/v2/resource' + resource;
+				} else if (version === 1) {
+					request['url'] += '/api/' + config.key + resource;
 				}
 			}
 
 			// HAS DATA? -> INSERT
-			if(data !== null) {
+			if (data !== null) {
 				request['data'] = data;
 			}
 
 			// RUN REQUEST
 			axios(request)
-			.then(function(response)
-			{
-				if(version === 2)
-				{
-					resolve(response.data.data);
-				}
-				else if(version === 1)
-				{
-					resolve(response.data);
-				}
-			})
-			.catch(function(error)
-			{
-				if (error.response)
-				{
-					reject({ status: error.response.status, errors: error.response.data.errors ? error.response.data.errors : error.response.data});
-				}
-				else
-				{
-					reject({ status: error.code, errors: error.message});
-				}
-			});
+				.then(function (response) {
+					if (version === 2) {
+						resolve(response.data.data);
+					} else if (version === 1) {
+						resolve(response.data);
+					}
+				})
+				.catch(function (error) {
+					if (error.response) {
+						reject({ status: error.response.status, errors: error.response.data.errors ? error.response.data.errors : error.response.data });
+					} else {
+						reject({ status: error.code, errors: error.message });
+					}
+				});
 		});
-	}
+	};
 
 	//
 	// SUBSCRIBE TO BRIDGE EVENTS
-	this.subscribe = function(config, callback)
-	{
+	this.subscribe = function (config, callback) {
 		const scope = this;
-		return new Promise(function(resolve, reject)
-		{
-			if(!scope.events[config.id])
-			{
-				var sseURL = "https://" + config.bridge + "/eventstream/clip/v2";
+		// eslint-disable-next-line no-unused-vars
+		return new Promise(function (resolve, reject) {
+			if (!scope.events[config.id]) {
+				let sseURL = 'https://' + config.bridge + '/eventstream/clip/v2';
 
 				// INITIALIZE EVENT SOURCE
 				scope.events[config.id] = new EventSource(sseURL, {
@@ -129,16 +108,12 @@ function API()
 				});
 
 				// PIPE MESSAGE TO TARGET FUNCTION
-				scope.events[config.id].onmessage = function(event)
-				{
-					if(event && event.type === 'message' && event.data)
-					{
+				scope.events[config.id].onmessage = function (event) {
+					if (event && event.type === 'message' && event.data) {
 						const messages = JSON.parse(event.data);
-						for (var i = messages.length - 1; i >= 0; i--)
-						{
+						for (let i = messages.length - 1; i >= 0; i--) {
 							const message = messages[i];
-							if(message.type === "update")
-							{
+							if (message.type === 'update') {
 								callback(message.data);
 							}
 						}
@@ -146,128 +121,120 @@ function API()
 				};
 
 				// CONNECTED?
-				scope.events[config.id].onopen = function()
-				{
+				scope.events[config.id].onopen = function () {
 					resolve(true);
-				}
+				};
 
 				// ERROR? -> RETRY?
-				scope.events[config.id].onerror = function(error)
-				{
-					console.log("HueMagic:", "Connection to bridge lost. Trying to reconnect again in 30 seconds…", error);
-					setTimeout(function(){ scope.subscribe(config, callback); }, 30000);
+				scope.events[config.id].onerror = function (error) {
+					console.log('HueMagic:', 'Connection to bridge lost. Trying to reconnect again in 30 seconds…', error);
+					setTimeout(function () {
+						scope.subscribe(config, callback);
+					}, 30000);
 					resolve(true);
-				}
-			}
-			else
-			{
+				};
+			} else {
 				scope.unsubscribe(config);
 				scope.subscribe(config, callback);
 			}
 		});
-	}
+	};
 
 	//
 	// UNSUBSCRIBE
-	this.unsubscribe = function(config)
-	{
-		if(this.events[config.id] !== null) { this.events[config.id].close(); }
+	this.unsubscribe = function (config) {
+		if (this.events[config.id] !== null) {
+			this.events[config.id].close();
+		}
 		this.events[config.id] = null;
-	}
+	};
 
 	//
 	// GET FULL/ROOT RESOURCE
-	this.fullResource = function(resource, allResources = {})
-	{
+	this.fullResource = function (resource, allResources = {}) {
 		const scope = this;
-		var fullResource = Object.assign({}, resource);
+		let fullResource = Object.assign({}, resource);
 
-		if(resource["owner"])
-		{
-			fullResource = scope.fullResource(allResources[fullResource["owner"]["rid"]], allResources);
-		}
-		else if(resource["type"] === "device" || resource["type"] === "room" || resource["type"] === "zone" || resource["type"] === "bridge_home")
-		{
+		if (resource['owner']) {
+			fullResource = scope.fullResource(allResources[fullResource['owner']['rid']], allResources);
+		} else if (resource['type'] === 'device' || resource['type'] === 'room' || resource['type'] === 'zone' || resource['type'] === 'bridge_home') {
 			// RESOLVE SERVICES
-			var allServices = {};
+			let allServices = {};
 
-			for (var i = resource["services"].length - 1; i >= 0; i--)
-			{
-				const targetService = resource["services"][i];
-				const targetType = targetService["rtype"];
-				const targetID = targetService["rid"];
+			for (let i = resource['services'].length - 1; i >= 0; i--) {
+				const targetService = resource['services'][i];
+				const targetType = targetService['rtype'];
+				const targetID = targetService['rid'];
 
-				if(!allServices[targetType]) { allServices[targetType] = {}; }
+				if (!allServices[targetType]) {
+					allServices[targetType] = {};
+				}
 				allServices[targetType][targetID] = Object.assign({}, allResources[targetID]);
 			}
 
 			// REPLACE SERVICES
-			fullResource["services"] = allServices;
+			fullResource['services'] = allServices;
 		}
 
 		return fullResource;
-	}
+	};
 
 	//
 	// PROCESS RESOURCES
-	this.processResources = function(resources)
-	{
+	this.processResources = function (resources) {
 		const scope = this;
 
 		// SET CURRENT DATE/TIME
 		const currentDateTime = dayjs().format();
 
 		// ACTION!
-		return new Promise(function(resolve, reject)
-		{
+		// eslint-disable-next-line no-unused-vars
+		return new Promise(function (resolve, reject) {
 			let resourceList = {};
 			let processedResources = {
-				_groupsOf: {}
+				_groupsOf: {},
 			};
 
 			// CREATE ID BASED OBJECT OF ALL RESOURCES
-			resources.forEach(function(resource, index)
-			{
+			// eslint-disable-next-line no-unused-vars
+			resources.forEach(function (resource, index) {
 				// IS BUTTON? -> REMOVE PREVIOUS STATE
-				if(resource.type === "button")
-				{
-					delete resource["button"];
+				if (resource.type === 'button') {
+					delete resource['button'];
 				}
 
 				resourceList[resource.id] = resource;
 			});
 
 			// GET FULL RESOURCES OF EACH OBJECT
-			resources.forEach(function(resource, index)
-			{
+			// eslint-disable-next-line no-unused-vars
+			resources.forEach(function (resource, index) {
 				// GET FULL RESOURCE
 				let fullResource = scope.fullResource(resource, resourceList);
 
 				// ADD CURRENT DATE/TIME
-				fullResource["updated"] = currentDateTime;
+				fullResource['updated'] = currentDateTime;
 
 				// ALL ALL TYPES BEHIND RESOURCE
-				fullResource["types"] = [ fullResource["type"] ];
+				fullResource['types'] = [fullResource['type']];
 
 				// RESOURCE HAS SERVICES?
-				if(fullResource["services"])
-				{
-					let additionalServiceTypes = Object.keys(fullResource["services"]);
+				if (fullResource['services']) {
+					let additionalServiceTypes = Object.keys(fullResource['services']);
 
 					// SET ADDITIONAL TYPES BEHIND RECCOURCE
-					fullResource["types"] = fullResource["types"].concat(additionalServiceTypes);
+					fullResource['types'] = fullResource['types'].concat(additionalServiceTypes);
 				}
 
 				// RESOURCE HAS GROUPED SERVICES?
-				if (fullResource["services"])
-				{
-					for (serviceType in fullResource["services"])
-					{
+				if (fullResource['services']) {
+					for (const serviceType in fullResource['services']) {
 						const grouped_services = fullResource['services'][serviceType];
-						for (groupedServiceID in grouped_services)
-						{
-							if (!processedResources["_groupsOf"][groupedServiceID]) { processedResources["_groupsOf"][groupedServiceID] = []; }
-							processedResources["_groupsOf"][groupedServiceID].push(fullResource.id);
+						for (const groupedServiceID in grouped_services) {
+							if (!processedResources['_groupsOf'][groupedServiceID]) {
+								processedResources['_groupsOf'][groupedServiceID] = [];
+							}
+							processedResources['_groupsOf'][groupedServiceID].push(fullResource.id);
 						}
 					}
 				}
@@ -278,8 +245,8 @@ function API()
 
 			resolve(processedResources);
 		});
-	}
+	};
 }
 
 // EXPORT
-module.exports = new API;
+module.exports = new API();
