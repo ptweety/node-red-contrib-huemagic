76f4f3d3868065ac1315ad68ce3953fb68a0d0d1
Matthias <me@ptweety.net>
2022-03-26 12:48:19 +0100

    Update hue-buttons.js
    linting with `eslint` and `prettier`

--- huemagic/hue-buttons.js
+++ huemagic/hue-buttons.js
@@ -1,9 +1,7 @@
-module.exports = function(RED)
-{
-	"use strict";
+module.exports = function (RED) {
+	'use strict';
 
-	function HueButtons(config)
-	{
+	function HueButtons(config) {
 		RED.nodes.createNode(this, config);
 
 		const scope = this;
@@ -17,41 +15,34 @@ module.exports = function(RED)
 
 		//
 		// CHECK CONFIG
-		if(bridge == null)
-		{
-			this.status({fill: "red", shape: "ring", text: "hue-buttons.node.not-configured"});
+		if (bridge == null) {
+			this.status({ fill: 'red', shape: 'ring', text: 'hue-buttons.node.not-configured' });
 			return false;
 		}
 
 		//
 		// UNIVERSAL MODE?
-		if(!config.sensorid)
-		{
-			this.status({fill: "grey", shape: "dot", text: "hue-buttons.node.universal"});
+		if (!config.sensorid) {
+			this.status({ fill: 'grey', shape: 'dot', text: 'hue-buttons.node.universal' });
 		}
 
 		//
 		// UPDATE STATE
-		if(config.sensorid)
-		{
-			scope.status({fill: "grey", shape: "dot", text: "hue-buttons.node.waiting"});
+		if (config.sensorid) {
+			scope.status({ fill: 'grey', shape: 'dot', text: 'hue-buttons.node.waiting' });
 		}
 
 		//
 		// SUBSCRIBE TO UPDATES FROM THE BRIDGE
-		bridge.subscribe("button", config.sensorid, function(info)
-		{
-			let currentState = bridge.get("button", info.id);
+		bridge.subscribe('button', config.sensorid, function (info) {
+			let currentState = bridge.get('button', info.id);
 
 			// RESOURCE FOUND?
-			if(currentState !== false)
-			{
+			if (currentState !== false) {
 				// SEND MESSAGE
-				if(!config.skipevents && currentState.payload.button !== false && (config.initevents || info.suppressMessage == false))
-				{
+				if (!config.skipevents && currentState.payload.button !== false && (config.initevents || info.suppressMessage == false)) {
 					// SET LAST COMMAND
-					if(scope.lastCommand !== null)
-					{
+					if (scope.lastCommand !== null) {
 						currentState.command = scope.lastCommand;
 					}
 
@@ -63,48 +54,44 @@ module.exports = function(RED)
 				}
 
 				// NOT IN UNIVERAL MODE? -> CHANGE UI STATES
-				if(config.sensorid)
-				{
-					if(currentState.payload.button === false)
-					{
-						scope.status({fill: "grey", shape: "dot", text: "hue-buttons.node.waiting"});
-					}
-					else
-					{
-						var action = "";
-						switch (currentState.payload.action)
-						{
-						  case "initial_press":
-						    action = "(pressed)";
-						    break;
-						  case "repeat":
-						    action = "(repeated)";
-						    break;
-						  case "short_release":
-						    action = "(short press)";
-						    break;
-						  case "long_release":
-						    action = "(long press)";
-						    break;
-						  case "double_short_release":
-						  	action = "(double pressed)";
-						  	break;
-						  default:
-						    action = "(pressed)";
+				if (config.sensorid) {
+					if (currentState.payload.button === false) {
+						scope.status({ fill: 'grey', shape: 'dot', text: 'hue-buttons.node.waiting' });
+					} else {
+						let action = '';
+						switch (currentState.payload.action) {
+							case 'initial_press':
+								action = '(pressed)';
+								break;
+							case 'repeat':
+								action = '(repeated)';
+								break;
+							case 'short_release':
+								action = '(short press)';
+								break;
+							case 'long_release':
+								action = '(long press)';
+								break;
+							case 'double_short_release':
+								action = '(double pressed)';
+								break;
+							default:
+								action = '(pressed)';
 						}
 
-						scope.status({fill: "blue", shape: "dot", text: "Button #"+ currentState.payload.button + " " + action });
+						scope.status({ fill: 'blue', shape: 'dot', text: 'Button #' + currentState.payload.button + ' ' + action });
 
 						// RESET TO WAITING AFTER 3 SECONDS
-						if(scope.timeout !== null) { clearTimeout(scope.timeout); };
-						scope.timeout = setTimeout(function()
-						{
-							scope.status({fill: "grey", shape: "dot", text: "hue-buttons.node.waiting"});
+						if (scope.timeout !== null) {
+							clearTimeout(scope.timeout);
+						}
+						scope.timeout = setTimeout(function () {
+							scope.status({ fill: 'grey', shape: 'dot', text: 'hue-buttons.node.waiting' });
 
 							// REMOVE OLD BUTTON STATES
-							for (const [oneButtonID, oneButton] of Object.entries(bridge.resources[config.sensorid]["services"]["button"]))
-							{
-								delete bridge.resources[config.sensorid]["services"]["button"][oneButtonID]["button"];
+							// eslint-disable-next-line no-unused-vars
+							for (const [oneButtonID, oneButton] of Object.entries(bridge.resources[config.sensorid]['services']['button'])) {
+								delete bridge.resources[config.sensorid]['services']['button'][oneButtonID]['button'];
 							}
 						}, 3000);
 					}
@@ -114,36 +101,46 @@ module.exports = function(RED)
 
 		//
 		// ON COMMAND
-		this.on('input', function(msg, send, done)
-		{
+		this.on('input', function (msg, send, done) {
 			// REDEFINE SEND AND DONE IF NOT AVAILABLE
-			send = send || function() { scope.send.apply(scope,arguments); }
-			done = done || function() { scope.done.apply(scope,arguments); }
+			// eslint-disable-next-line no-unused-vars
+			send =
+				send ||
+				function () {
+					scope.send.apply(scope, arguments);
+				};
+			done =
+				done ||
+				function () {
+					scope.done.apply(scope, arguments);
+				};
 
 			// SAVE LAST COMMAND
 			scope.lastCommand = RED.util.cloneMessage(msg);
 
 			// DEFINE SENSOR ID
-			const tempSensorID = (!config.sensorid && typeof msg.topic != 'undefined' && bridge.validResourceID.test(msg.topic) === true) ? msg.topic : config.sensorid;
-			if(!tempSensorID)
-			{
-				scope.error("Please submit a valid button ID.");
+			const tempSensorID =
+				!config.sensorid && typeof msg.topic != 'undefined' && bridge.validResourceID.test(msg.topic) === true ? msg.topic : config.sensorid;
+			if (!tempSensorID) {
+				scope.error('Please submit a valid button ID.');
 				return false;
 			}
 
-			let currentState = bridge.get("button", tempSensorID);
-			if(!currentState)
-			{
-				scope.error("The button/switch in not yet available. Please wait until HueMagic has established a connection with the bridge or check whether the resource ID in the configuration is valid.");
+			let currentState = bridge.get('button', tempSensorID);
+			if (!currentState) {
+				scope.error(
+					'The button/switch in not yet available. Please wait until HueMagic has established a connection with the bridge or check whether the resource ID in the configuration is valid.'
+				);
 				return false;
 			}
 
 			// GET CURRENT STATE
-			if( (typeof msg.payload != 'undefined' && typeof msg.payload.status != 'undefined') || (typeof msg.__user_inject_props__ != 'undefined' && msg.__user_inject_props__ == "status") )
-			{
+			if (
+				(typeof msg.payload != 'undefined' && typeof msg.payload.status != 'undefined') ||
+				(typeof msg.__user_inject_props__ != 'undefined' && msg.__user_inject_props__ == 'status')
+			) {
 				// SET LAST COMMAND
-				if(scope.lastCommand !== null)
-				{
+				if (scope.lastCommand !== null) {
 					currentState.command = scope.lastCommand;
 				}
 
@@ -153,11 +150,13 @@ module.exports = function(RED)
 				// RESET LAST COMMAND
 				scope.lastCommand = null;
 
-				if(done) { done(); }
+				if (done) {
+					done();
+				}
 				return true;
 			}
 		});
 	}
 
-	RED.nodes.registerType("hue-buttons", HueButtons);
-}
+	RED.nodes.registerType('hue-buttons', HueButtons);
+};
